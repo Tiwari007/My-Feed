140df808524e318e6280e33678e049dbf5fce6f0
Tiwari007 <vt2174781@gmail.com>
2023-11-08 12:42:56 +0530

    (feat): added my all projects link to this project

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const currentUser = localStorage.getItem("currentUser");
 const Header = () => {
   return (
     <div className='text-center mt-6 flex justify-between'>
-      <p className='text-5xl '>Stantech AI</p>
+      <p className='text-5xl '>Stantech AI<a href="https://projectsbyvivektiwari.netlify.app/" target="_blank" rel="noreferrer" className='text-3xl text-green-900'><button className='ml-10 mr-4 p-2 px-6 bg-red-100'>Click on Me</button></a></p>
       <div className='flex'>
         <Link to="/"><button className='mr-4 p-2 px-6 bg-yellow-100 text-black'>HOME</button></Link>
         <Link to="/users"><button className='mr-4 p-2 px-6 bg-yellow-100 text-black'>USERS</button></Link>
